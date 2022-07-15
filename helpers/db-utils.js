cd36876f4582ec7e2f45bf7ce9bcead1cbb58bc9
mpoweredo <mpow3r1337@gmail.com>
2022-07-15 13:17:21 +0200

    fixing process.env

--- helpers/db-utils.js
+++ helpers/db-utils.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb'
 
 export const connectToDatabase = async () => {
-    const client = await MongoClient.connect(process.mongodbURL)
+    const client = await MongoClient.connect(process.env.mongodbURL)
     return client
 }
